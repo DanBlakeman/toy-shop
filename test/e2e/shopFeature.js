0ef3124f47cb999e07ab2130eb2aa05cc282bfd5
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-06-15 12:19:24 +0100

    controller returns items in json

--- test/e2e/shopFeature.js
+++ test/e2e/shopFeature.js
@@ -5,4 +5,10 @@ describe('A shop toy app', function () {
     expect(browser.getTitle()).toEqual('Hello World!');
   });
 
+  it('lists products with their category, price & availability', function () {
+    browser.get('http://localhost:8080');
+     expect(element.all(by.repeater('item in ShopApp.items')).get(0).getText()).toContain('Almond Toe Court Shoes, Patent Black');
+  });
+
+
 });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,8 +19,8 @@ module.exports = function (config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'test/**/*Spec.js',
-      'js/*.js'
+      'js/**/*.js',
+      'test/**/*Spec.js'
     ],
 
 
