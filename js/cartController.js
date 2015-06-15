2a404d433280831f49621707ca0548769f0bf773
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-06-15 21:41:47 +0100

    user can add to their basket

--- js/cartController.js
+++ js/cartController.js
@@ -2,11 +2,18 @@ shopApp.controller('CartController', ['ProductList', function(ProductList) {
 
   var self = this;
 
-  self.items = [];
+  self.addedItems = [];
+
+  self.show = false;
+
+  self.listItems = function () {
+    return self.addedItems;
+  }
 
   self.addToCart = function (product_index) {
     fetched_items = ProductList.items();
-    self.items.push(fetched_items[product_index]);
+    self.addedItems.push(fetched_items[product_index]);
+    self.show = true;
   };
 
 
--- test/e2e/cartFeature.js
+++ test/e2e/cartFeature.js
@@ -3,10 +3,9 @@ describe('A shop toy app', function () {
   it('allows user to add product to cart', function () {
     browser.get('http://localhost:8080');
     items = element.all(by.repeater('item in Main.items'));
-   items.get(0).$('#checkoutBtn').click();
-   expect($('#cart').isDisplayed()).toEqual(true);
-   itemsInCart = element.all(by.repeater('item in Cart.Addeditems'));
-   expect(itemsInCart.get(0).getText()).toContain('Almond Toe Court Shoes, Patent Black');
+    items.get(0).$('#checkoutBtn').click();
+    expect($('#cart').isDisplayed()).toEqual(true);
+    expect(element.all(by.repeater('item in Cart.addedItems')).get(0).getText()).toContain('Almond Toe Court Shoes, Patent Black');
   });
 
 });
