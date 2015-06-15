cb9f19bf2ac419b4d7e8188cf427197361bb7ee1
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-06-15 17:30:18 +0100

    can add item from productlist to cart

--- js/cartController.js
+++ js/cartController.js
@@ -1,11 +1,12 @@
-shopApp.controller('CartController', [function() {
+shopApp.controller('CartController', ['ProductList', function(ProductList) {
 
   var self = this;
 
   self.items = [];
 
   self.addToCart = function (product_index) {
-
+    fetched_items = ProductList.items();
+    self.items.push(fetched_items[product_index]);
   };
 
 
--- test/CartControllerSpec.js
+++ test/CartControllerSpec.js
@@ -1,19 +1,48 @@
-// describe('CartController', function () {
-//   beforeEach(module('ShopApp'));
-//   var ctrl;
+describe('CartController', function () {
+  beforeEach(module('ShopApp'));
+  var ctrl;
 
-//   beforeEach(inject(function($controller) {
-//     ctrl = $controller('CartController');
-//   }));
+  beforeEach(function() {
 
-//   it('initialises with cart empty of items', function() {
-//     expect(ctrl.items).toEqual([]);
-//   });
+  productListFactoryMock = {
+      items: function() {
+        return [
+    { "Product Name": "Almond Toe Court Shoes, Patent Black", "Category": "Women’s Footwear", "Price": 99.00, "Quantity in Stock": 5},
+    { "Product Name": "Suede Shoes, Blue","Category": "Women’s Footwear", "Price": 42.00, "Quantity in Stock": 44},
+    { "Product Name": "Leather Driver Saddle Loafers, Tan", "Category": "Men’s Footwear", "Price": 34.00, "Quantity in Stock": 12},
+    { "Product Name": "Flip Flops, Red", "Category": "Men’s Footwear", "Price": 19.00, "Quantity in Stock": 6},
+    { "Product Name": "Flip Flops, Blue", "Category": "Men’s Footwear", "Price": 19.00, "Quantity in Stock": 0},
+    { "Product Name": "Gold Button Cardigan, Black", "Category": "Women’s Casualwear", "Price": 167.00, "Quantity in Stock": 6},
+    { "Product Name": "Cotton Shorts, Medium Red", "Category": "Women’s Casualwear", "Price": 30.00, "Quantity in Stock": 5},
+    { "Product Name": "Fine Stripe Short Sleeve Shirt, Grey", "Category": "Men’s Casualwear", "Price": 49.99, "Quantity in Stock": 9},
+    { "Product Name": "Fine Stripe Short Sleeve Shirt, Green", "Category": "Men’s Casualwear", "Price": 39.99, "Quantity in Stock": 3},
+    { "Product Name": "Sharkskin Waistcoat, Charcoal", "Category": "Men’s Formalwear", "Price": 75.00, "Quantity in Stock": 2},
+    { "Product Name": "Lightweight Patch Pocket￼Blazer, Deer", "Category": "Men’s Formalwear", "Price": 175.50, "Quantity in Stock": 1},
+    { "Product Name": "Bird Print Dress, Black", "Category": "Women’s Formalwear", "Price": 270.00, "Quantity in Stock": 10},
+    { "Product Name": "Mid Twist Cut-Out Dress, Pink", "Category": "Women’s Formalwear", "Price": 540.00, "Quantity in Stock": 5}
+];
+      }
+    };
+
+    module(function($provide) {
+      $provide.value('ProductList', productListFactoryMock);
+    });
+
+  });
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('CartController');
+  }));
 
 
-//   it('can add an item to cart given a product index', function() {
-//     ctrl.addToCart(0);
-//     expect(ctrl.items).toEqual([{ "Product Name": "Almond Toe Court Shoes, Patent Black", "Category": "Women’s Footwear", "Price": 99.00, "Quantity in Stock": 5}]);
-//   });
+  it('initialises with cart empty of items', function() {
+    expect(ctrl.items).toEqual([]);
+  });
 
-// });
+
+  it('can add an item to cart given a product index', function() {
+    ctrl.addToCart(0);
+    expect(ctrl.items).toEqual([{ "Product Name": "Almond Toe Court Shoes, Patent Black", "Category": "Women’s Footwear", "Price": 99.00, "Quantity in Stock": 5}]);
+  });
+
+});
